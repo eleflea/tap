7529d67d832276cdf70ec23895cb2a3beae7f95d
eleflea <dmzitnzc@gmail.com>
2025-02-16 22:15:10 +1100

    Initial commit

---
+++ test/tap.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tap from '../lib/tap-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tap-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tap.TapStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
