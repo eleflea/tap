d8a5d902e4912db63299b7133c7238d5f33feebf
eleflea <dmzitnzc@gmail.com>
2025-03-26 12:46:08 +1100

    manage multiple chat sessions with improved state handling

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -6,6 +6,11 @@ interface Message {
   content: string;
 }
 
+interface Chat {
+  id: number;
+  messages: Message[];
+}
+
 const chatApi = process.env.REACT_APP_API_URL ?? "";
 
 const getResponse = async (
@@ -43,13 +48,35 @@ const getResponse = async (
 };
 
 const Home = () => {
-  const [chats, setChats] = useState<Message[]>([]);
+  const [chats, setChats] = useState<Chat[]>([]);
+  const [activeChatId, setActiveChatId] = useState<number>(1);
   const [input, setInput] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isResponsing, setIsResponsing] = useState(false);
+  const activeChat = chats.find((chat) => chat.id === activeChatId);
+
+  if (chats.length === 0) {
+    setChats([{ id: 1, messages: [] }]);
+  }
+
+  const setActiveChat = (func: (prev: Message[]) => Message[]) => {
+    setChats(
+      chats.map((chat) => {
+        if (chat.id === activeChatId) {
+          chat.messages = func(chat.messages);
+        }
+        return chat;
+      })
+    );
+  };
+
+  const newChat = () =>
+    setChats([...chats, { id: chats.length + 1, messages: [] }]);
+  const hasNewChat =
+    chats.find((chat) => chat.messages.length === 0) !== undefined;
 
   const appendResponse = (delta: string, isEnded?: boolean) => {
-    setChats((prev) => {
+    setActiveChat((prev) => {
       if (prev.at(-1)?.content === "") {
         setIsLoading(false);
       }
@@ -68,66 +95,94 @@ const Home = () => {
     if (!input.trim()) return;
 
     const userMessage = { role: "user", content: input } as Message;
-    const nextChats = [
-      ...chats,
-      userMessage,
-      { role: "assistant", content: "" } as Message,
-    ];
-    setIsLoading(true);
-    setIsResponsing(true);
-    setChats(nextChats);
-    setInput("");
+    const messages = activeChat?.messages;
+    if (messages) {
+      const nextMessages = [
+        ...messages,
+        userMessage,
+        { role: "assistant", content: "" } as Message,
+      ];
+      setIsLoading(true);
+      setIsResponsing(true);
+      setActiveChat((prev) => nextMessages);
+      setInput("");
 
-    getResponse(nextChats, appendResponse);
+      getResponse(nextMessages, appendResponse);
+    }
   };
 
   return (
     <div className="w-full h-screen max-h-screen flex flex-col justify-center items-center bg-gray-100 gap-6">
       <h1 className="text-2xl font-bold text-gray-800">Your Hacker Friend</h1>
-      <div className="bg-white shadow-lg rounded-xl p-4 flex flex-col w-4/5 h-4/5">
-        <div className="flex-1 overflow-y-auto p-2 space-y-2">
-          {chats.map((chat, index) => (
-            <div
-              key={index}
-              className={`flex ${
-                chat.role === "user" ? "justify-end" : "justify-start"
+      <div className="bg-white shadow-lg rounded-xl flex w-4/5 h-4/5">
+        <div className="w-48 flex flex-col gap-1 bg-gray-50 p-4">
+          {chats.map((chat) => (
+            <button
+              key={chat.id}
+              className={`p-2 rounded-lg text-left truncate h-10 ${
+                chat.id === activeChatId
+                  ? "bg-gray-200 text-gray-800"
+                  : "text-gray-800"
               }`}
+              onClick={() => setActiveChatId(chat.id)}
             >
-              <div
-                className={`p-2 rounded-lg max-w-3xl ${
-                  chat.role === "user"
-                    ? "bg-blue-500 text-white"
-                    : "bg-gray-200 text-gray-800"
-                }`}
-              >
-                {isLoading && index === chats.length - 1 && (
-                  <div className="flex space-x-1 h-4 justify-center items-center">
-                    <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce"></div>
-                    <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce delay-100"></div>
-                    <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce delay-200"></div>
-                  </div>
-                )}
-                <ReactMarkdown>{chat.content}</ReactMarkdown>
-              </div>
-            </div>
+              {chat.messages.at(0)?.content ?? "New Chat"}
+            </button>
           ))}
         </div>
-        <div className="flex items-center p-2 border-t">
-          <input
-            type="text"
-            className="flex-1 p-2 border rounded-lg outline-none"
-            placeholder="Type a message..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSend()}
-          />
-          <button
-            className="ml-2 p-2 bg-blue-500 text-white rounded-lg disabled:bg-gray-300"
-            onClick={handleSend}
-            disabled={isResponsing}
-          >
-            Send
-          </button>
+        <div className="flex flex-col grow p-4">
+          <div className="flex-1 overflow-y-auto p-2 space-y-2">
+            {activeChat &&
+              activeChat.messages.map((chat, index) => (
+                <div
+                  key={index}
+                  className={`flex ${
+                    chat.role === "user" ? "justify-end" : "justify-start"
+                  }`}
+                >
+                  <div
+                    className={`p-2 rounded-lg max-w-3xl ${
+                      chat.role === "user"
+                        ? "bg-blue-500 text-white"
+                        : "bg-gray-200 text-gray-800"
+                    }`}
+                  >
+                    {isLoading && index === activeChat.messages.length - 1 && (
+                      <div className="flex space-x-1 h-4 justify-center items-center">
+                        <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce"></div>
+                        <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce delay-100"></div>
+                        <div className="w-1 h-1 bg-gray-500 rounded-full animate-bounce delay-200"></div>
+                      </div>
+                    )}
+                    <ReactMarkdown>{chat.content}</ReactMarkdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div className="flex items-center py-2 border-t">
+            <button
+              className="mr-2 p-2 bg-blue-500 text-white rounded-lg disabled:bg-gray-300"
+              onClick={newChat}
+              disabled={hasNewChat || isResponsing}
+            >
+              New
+            </button>
+            <input
+              type="text"
+              className="flex-1 p-2 border rounded-lg outline-none"
+              placeholder="Type a message..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && handleSend()}
+            />
+            <button
+              className="ml-2 p-2 bg-blue-500 text-white rounded-lg disabled:bg-gray-300"
+              onClick={handleSend}
+              disabled={isResponsing}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </div>
